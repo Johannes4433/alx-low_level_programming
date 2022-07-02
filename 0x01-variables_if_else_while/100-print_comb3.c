d1085851261abe3d8ed6114237c18f73b1e44533
johannes4433 <yohannesg4433@gmail.com>
2022-07-01 19:42:18 -0700

    The advanced 100 task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -24,13 +24,13 @@ int main (void)
 			{
 				putchar(ch);
 				putchar(n);
-				if (ch != 56||n != 57)
+				if (ch != 56 || n != 57)
 				{
 					putchar(44);
 					putchar(32);
 				}
-			}						
-		}		
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
