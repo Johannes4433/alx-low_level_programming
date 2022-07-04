f15b0d17ed43e59e7ee05a3f0a03690257c33dc1
johannes4433 <yohannesg4433@gmail.com>
2022-07-04 06:40:00 -0700

    Review once again the positive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,35 +4,18 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 int main(void)
-
 {
-
-	        int n;
-
-		        srand(time(0));
-
-			        n = rand() - RAND_MAX / 2;
-
-				        int q;
-
-					        q=n%10;
-
-						        q=abs(q);
-
-							        if(q >5)
-
-									                printf("Last digit of %d is %d and is greater than 5\n",n,q);
-
-								        else if(q==0)
-
-										                printf("Last digit of %d is %d and is 0\n",n,q);
-
-									        else if(q<6 && q!=0)
-
-											                printf("Last digit of %d is %d and is less than 6 and not 0\n",n,q);
-
-										        return (0);
-
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int q;
+	q=n%10;
+	q=abs(q);
+	if(q >5)
+		printf("Last digit of %d is %d and is greater than 5\n",n,q);
+	else if(q==0)
+		printf("Last digit of %d is %d and is 0\n",n,q);
+	else if(q<6 && q!=0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",n,q);
+	return (0);
 }
-
-
